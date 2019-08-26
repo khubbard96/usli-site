4f90e8aa27a6a47463e5bb147264d73666aeb6ac
khubbard96 <kevlar2020@gmail.com>
2019-08-26 14:01:40 -0500

    asdf

--- admin/app.js
+++ admin/app.js
@@ -1,3 +1,9 @@
+//some util functions
+Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+};
+
+
 //pull site data
 function loadData() {
     let xhttp = new XMLHttpRequest();
@@ -12,13 +18,14 @@ function loadData() {
     xhttp.open("GET", filename, true);
     xhttp.send();
 };
-var app;
+var teamInformationApp;
 window.onload = function() {
-    app = new Vue({
-        el: "#app_container",
+    teamInformationApp = new Vue({
+        el: "#team_info_container",
         data: {
             teams: [],
             unsavedChanges: false,
+            originalData: [],
         },
         methods: {
             addTeam: function() {
@@ -30,46 +37,80 @@ window.onload = function() {
                     "desc": "",
                     "image": ""
                 };
-                this.teams.push(newTemplate);
+                this.teams.push(newTemplate)
             },
             removeTeam: function(idx) {
+                let removingItem;
                 if(this.teams.length == 1) {
+                    removingItem = this.teams[0];
                     this.teams = [];
                 }
                 else {
-                    this.teams.splice(idx, 1);
+                    removingItem = this.teams.splice(idx, 1)[0];
+                }
+                var toastHTML = '<span>Team Lead Removed</span><button class="btn-flat toast-action undo-remove">Undo</button>';
+                M.toast({html: toastHTML, classes: "toast", completeCallback: () => { removingItem = undefined; }});
+                let self = this;
+                document.querySelector(".undo-remove").onclick = () => {
+                    if(removingItem) {
+                        self.teams.push(removingItem);
+                        Vue.nextTick(() => {
+                            document.querySelector('[data-id="' + (self.teams.length-1) +'"]').scrollIntoView();
+                        });
+                        removingItem = undefined;
+                    }
                 }
             },
             saveChanges: function() {
                 this.unsavedChanges = false;
+            },
+            moveTeamUp: function(currentIdx) {
+                if(currentIdx == 0) return;
+                this.teams.move(currentIdx, currentIdx - 1);
+            },
+            moveTeamDown: function(currentIdx) {
+                if(currentIdx >= this.teams.length) return;
+                this.teams.move(currentIdx, currentIdx + 1);
+            },
+            discardChanges: function() {
+                loadData();
+            },
+            changeYear: function(event) {
+                let value = event.target.value;
+                console.log(value);
             }
         },
         watch: {
             teams:{
                 handler: function(val) {
-                    app.unsavedChanges = true;
-                    var elems = document.querySelectorAll('.tooltipped');
-                    var instances = M.Tooltip.init(elems, {});
+                    teamInformationApp.unsavedChanges = true;
                 },
                 deep: true,
             }
         },
-        mounted: function() {
-            console.log(this.$refs.tooltipped);
-        }
     });
     loadData();
-
+    let tabEls = document.querySelector(".tabs");
+    let tabs = M.Tabs.init(tabEls, {});
+    tabEls.select("general");
 }
 function updateView(data) {
-    app.teams = data;
+    teamInformationApp.teams = data;
+    Vue.nextTick(() => {
+        teamInformationApp.unsavedChanges = false;
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, {});
+    });
+}
+function undoRemove(item) {
+    if(item) teamInformationApp.teams.push(item);
 }
 
 
 /* function loadSite() {
-    var app = angular.module('adminApp', []);
+    var teamInformationApp = angular.module('adminApp', []);
 
-    app.controller('TeamInfoController', ['$scope', function($scope) {
+    teamInformationApp.controller('TeamInfoController', ['$scope', function($scope) {
         $scope.teamData = [];
         if(siteData.siteData) {
             let thisYearData = siteData.siteData["2019"];
